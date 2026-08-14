e390d591eb5429827e65821d146397098a35c5c5
trier442 <koreangap@naver.com>
2026-08-14 17:25:36 +0900

    Expose desktop model manager to ADE UI

--- desktop/preload.cjs
+++ desktop/preload.cjs
@@ -1,4 +1,4 @@
-const { contextBridge } = require('electron');
+const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('ADEDesktop', Object.freeze({
   isDesktop: true,
@@ -8,4 +8,12 @@ contextBridge.exposeInMainWorld('ADEDesktop', Object.freeze({
     chrome: process.versions.chrome,
     electron: process.versions.electron,
   }),
+  modelStatus: () => ipcRenderer.invoke('ade:model-status'),
+  downloadModel: () => ipcRenderer.invoke('ade:model-download'),
+  onModelProgress: callback => {
+    if (typeof callback !== 'function') return () => {};
+    const listener = (_event, payload) => callback(payload);
+    ipcRenderer.on('ade:model-progress', listener);
+    return () => ipcRenderer.removeListener('ade:model-progress', listener);
+  },
 }));
